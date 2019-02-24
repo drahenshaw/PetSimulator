695c33c73fbe91acc13c798a025ef9d469c0bff9
mkilgore2000 <michael.kilgore@wsu.edu>
2019-02-23 22:33:26 -0800

    Background Class

---
+++ PetSimulatorSFML/PetSimulator/Background.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 600;
+
+
+class Background
+{
+public:
+	Background();
+	void renderBackground(sf::RenderWindow & window, sf::Sprite &Hamburger, sf::Sprite &Water,
+		sf::Sprite &grass, sf::Sprite &toolBar);
+	void drawBackground(sf::RenderWindow &window, sf::Sprite &Hamburger, sf::Sprite &Water,
+		sf::Sprite &grass, sf::Sprite &toolBar);
+	void renderGrass(sf::Sprite &grass);
+	void renderToolBar(sf::Sprite &toolBar);
+	void Background::renderConstantConsumables(sf::Sprite &Hamburger, sf::Sprite &Water);
+
+private:
+	//private textures
+	sf::Texture grassTexture;
+	sf::Texture toolBarTexture;
+	sf::Texture HamburgerTexture;
+	sf::Texture WaterTexture;
+	
+
+};
--- PetSimulatorSFML/PetSimulator/Source.cpp
+++ PetSimulatorSFML/PetSimulator/Source.cpp
@@ -1,47 +1,26 @@
 #include <iostream>
 
+#include "Background.h"
 #include "Creature.h"
 #include "Health_Bars.h"
 #include "Consumables.h"
 
+
 int main(void)
 {
-	//Screen dimensions
-	constexpr int SCREEN_WIDTH = 800;
-	constexpr int SCREEN_HEIGHT = 600;
-
+	sf::Sprite Water, grass, Hamburger, toolBar;
 	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "SFML works!");
-	sf::Texture texture;
+	Background background;
+	background.renderBackground(window, Hamburger, Water, grass, toolBar);
 	health_bars bars;
-	sf::Texture texture2;
-	texture.loadFromFile("Assets/grass block.jpg");
-	texture2.loadFromFile("Assets/userInterface.png");
 	Consumables consume;
-	//sf::Texture texture2 = consume.getTexture
-	sf::Texture temp = consume.getTexture(0);
-	sf::Sprite Hamburger(temp);
-	Hamburger.setScale(0.05, 0.05);
-	sf::Texture temp2 = consume.getTexture(1);
-	sf::Sprite Water(temp2);
-	Water.setScale(0.05, 0.05);
 
 	sf::Texture creature;
 	creature.loadFromFile("Assets/Creature 1.png");
 	sf::Sprite orcSprite(creature);
 	Creature * Orc = new Creature(150, 150, 150, 150, orcSprite);
 
-	texture.loadFromFile("Assets/grass block.jpg");
-	
 
-	sf::Sprite sprite(texture);
-	sf::Sprite sprite2(texture2);
-	sprite2.setScale(0.417246, 0.071633);
-
-	sprite.setTextureRect({ 20, 20, 100, 100 });
-
-	// Run windowasdf
-	
-	sprite.setTextureRect({ 20, 20, 100, 100 });
 	// Run window
 
 	while (window.isOpen())
@@ -58,24 +37,7 @@ int main(void)
 		window.clear();
 
 		// SETTING THE BACKGROUND////////////
-		//window.draw(background.getSprite());
-		for (int i = 0; i < 8; i++)
-		{
-			for (int j = 0; j < 6; j++)
-			{
-				sprite.setPosition(i * 100, j * 100);
-				window.draw(sprite);
-			}
-		}
-		sprite.setPosition(0, 0);
-
-		sprite2.setPosition(100, 500);
-		window.draw(sprite2);
-
-		Water.setPosition(300, 550);
-		Hamburger.setPosition(500, 550);
-		window.draw(Water);
-		window.draw(Hamburger);
+		background.drawBackground(window, Hamburger, Water, grass, toolBar);
 
 		Orc->Render(window);
 		window.draw(bars.getEnergy());
